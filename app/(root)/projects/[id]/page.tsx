9abb7899a5afa8a361f5e4c22b7c5f21aefd9a75
alexvieru1 <vieru.alexandruu@gmail.com>
2024-02-23 17:46:16 +0200

    99% done

--- app/(root)/projects/[id]/page.tsx
+++ app/(root)/projects/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -12,39 +13,86 @@ interface queryParamsTypes {
   details: string;
   createdAt: string;
 }
-
 const ReadMore = ({ searchParams }: { searchParams: queryParamsTypes }) => {
   const project = searchParams;
   return (
-    <div className="flex min-h-[90vh] flex-col p-24 4xs:mx-[-50px] md:mx-auto">
+    <div className="select-none flex min-h-[90vh] flex-col p-24 4xs:mx-[-50px] md:mx-auto">
       <div className="w-[95%] flex justify-center mb-5">
         <Image
           src={project.image}
           width={1000}
           height={600}
           alt="image name from public folder"
-          objectFit="cover"
+          className="rounded-lg border-[3px] border-white/[0.2]"
         />
       </div>
-      <h2 className="text-4xl font-bold uppercase text-center mt-10 text-gray-500 my-3">
+      <h2 className="text-xl font-bold uppercase text-center mt-10 text-gray-300 my-3">
         {project.title}
       </h2>
       <div className="flex md:gap-20 4xs:gap-5 relative mt-10 md:flex-row 4xs:flex-col">
         <article>
-          <p className="text-xl">{project.details}</p>
-          <div className="mt-5 flex gap-5 items-center">
-              <span className="text-gray-400 text-sm italic">
-                {project.createdAt}
-              </span>
+          <p className="text-md">{project.details}</p>
+          <p className="mt-2 text-md font-semibold text-gray-300 text-center">
+            {" "}
+            Technologies used:{" "}
+          </p>
+          <div className="flex justify-center p-6 items-center">
+            {project.technologies.map((p, index) => (
+              <Image
+                className="mx-2"
+                key={index}
+                src={p}
+                width={50}
+                height={50}
+                alt="technology"
+              />
+            ))}
+          </div>
+          {project.contributors && (
+            <div className="inline-flex">
+              <p className="mt-2 text-md italic text-gray-300 text-start mr-3">
+                {" "}
+                {Array.isArray(project.contributors) &&
+                project.contributors.length > 1
+                  ? "Contributors:"
+                  : "Contributor:"}
+              </p>
+              <p className="mt-2 text-md italic text-gray-300 text-start">
+                {Array.isArray(project.contributors)
+                  ? project.contributors.join(", ")
+                  : project.contributors}
+              </p>
+            </div>
+          )}
 
+          <div className="mt-5 flex gap-5 items-center">
+            <span className="text-gray-400 text-sm italic">
+              Project&apos;s release date: {project.createdAt}
+            </span>
           </div>
         </article>
       </div>
-      <Link href="/#projects">
-        <div className="mt-10 w-full flex items-center justify-center">
-          <button className="px-3 py-2">GitHub</button>
-        </div>
-      </Link>
+      <div className="flex justify-between mt-4">
+        <Link href="/#projects">
+          <button className="inline-flex h-12 animate-shimmer items-center justify-between rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <ChevronLeftIcon className="w-5 h-5" />
+            Projects
+          </button>
+        </Link>
+        <Link href={project.github} target="_blank">
+          <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <Image
+              src="/images/github-mark-white.svg"
+              width={20}
+              height={20}
+              alt="githublogo"
+              className="mr-2"
+              draggable={false}
+            />
+            Github
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
--- constants/projectsData.ts
+++ constants/projectsData.ts
@@ -14,8 +14,8 @@ export const projectsData = [
       "/images/technologies/node-js.svg",
     ],
     details:
-      "Introducing a showcase of my latest website project! This portfolio piece encapsulates my dedication to crafting captivating digital experiences that resonate with audiences. From concept to execution, I poured my creativity and expertise into every pixel, ensuring that the final product not only meets but exceeds expectations. This project demonstrates my ability to blend cutting-edge design with seamless functionality, resulting in a website that is both visually stunning and user-friendly. Dive in to explore how I brought this vision to life and discover the transformative impact it can have on your digital presence.",
-    createdAt: "1st of May 2023",
+      "Introducing my latest project: a bespoke presentation website crafted for real estate developer Mr. Lucass, based in Constanta, Romania. This comprehensive platform showcases Lucass's premier properties, construction stages, strategic location insights, and detailed about and contact sections. From elegant apartment layouts to dynamic construction updates, this website encapsulates Lucass's commitment to excellence in the realm of modern urban living, offering a seamless digital experience for potential clients.",
+    createdAt: "15th of January 2024",
   },
   {
     id: 2,
@@ -30,8 +30,8 @@ export const projectsData = [
       "/images/technologies/express-js.svg",
     ],
     details:
-      "Introducing a showcase of my latest website project! This portfolio piece encapsulates my dedication to crafting captivating digital experiences that resonate with audiences. From concept to execution, I poured my creativity and expertise into every pixel, ensuring that the final product not only meets but exceeds expectations. This project demonstrates my ability to blend cutting-edge design with seamless functionality, resulting in a website that is both visually stunning and user-friendly. Dive in to explore how I brought this vision to life and discover the transformative impact it can have on your digital presence.",
-    createdAt: "1st of May 2023",
+      "Presenting our collaborative triumph: a dynamic pizza ordering website developed by our talented team. With a user-friendly interface, enticing menu options, and seamless shopping cart functionality, our platform redefines the pizza ordering experience. From customizable toppings to swift checkout processes, every aspect of this website is meticulously designed to delight pizza enthusiasts and streamline the ordering process. Join us on a journey of culinary convenience and satisfaction as we revolutionize the way people enjoy their favorite slices, one click at a time.",
+    createdAt: "19th of April 2023",
   },
   {
     id: 3,
@@ -49,8 +49,8 @@ export const projectsData = [
       "/images/technologies/api.webp",
     ],
     details:
-      "Introducing a showcase of my latest website project! This portfolio piece encapsulates my dedication to crafting captivating digital experiences that resonate with audiences. From concept to execution, I poured my creativity and expertise into every pixel, ensuring that the final product not only meets but exceeds expectations. This project demonstrates my ability to blend cutting-edge design with seamless functionality, resulting in a website that is both visually stunning and user-friendly. Dive in to explore how I brought this vision to life and discover the transformative impact it can have on your digital presence.",
-    createdAt: "1st of May 2023",
+      "Introducing our collaborative triumph: a cutting-edge website for home and condo rentals, leveraging Airbnb's API to offer seamless bookings, authentic reviews, and personalized user accounts. Harnessing the power of Airbnb's expansive database, our platform provides users with unparalleled access to a diverse range of rental options, coupled with intuitive booking processes and secure transactions. With robust review functionalities integrated seamlessly, guests can make informed decisions, while hosts can showcase their properties with confidence. Join us as we redefine the rental landscape, merging innovation with the trusted infrastructure of Airbnb to deliver an unparalleled experience in the world of home sharing.",
+    createdAt: "19th of May 2023",
   },
   {
     id: 4,
@@ -66,8 +66,8 @@ export const projectsData = [
       "/images/technologies/api.webp",
     ],
     details:
-      "Introducing a showcase of my latest website project! This portfolio piece encapsulates my dedication to crafting captivating digital experiences that resonate with audiences. From concept to execution, I poured my creativity and expertise into every pixel, ensuring that the final product not only meets but exceeds expectations. This project demonstrates my ability to blend cutting-edge design with seamless functionality, resulting in a website that is both visually stunning and user-friendly. Dive in to explore how I brought this vision to life and discover the transformative impact it can have on your digital presence.",
-    createdAt: "1st of May 2023",
+      "Behold our team's imaginative creation: a thrilling website inspired by the Pokémon universe, where players embark on a real-world adventure to capture Pokémon through strategic duels. Leveraging the power of Pokémon's API, our platform seamlessly integrates location-based gameplay, challenging players to travel to various locations and engage in thrilling battles with wild Pokémon. With each victorious duel, players have the exhilarating opportunity to capture their defeated opponent, adding them to their collection. Join us on this epic journey as we blend the virtual realm with reality, bringing the magic of Pokémon to life in an innovative and immersive gaming experience.",
+    createdAt: "4th of May 2023",
   },
   {
     id: 5,
@@ -85,8 +85,8 @@ export const projectsData = [
       "/images/technologies/postgres.svg",
     ],
     details:
-      "Introducing a showcase of my latest website project! This portfolio piece encapsulates my dedication to crafting captivating digital experiences that resonate with audiences. From concept to execution, I poured my creativity and expertise into every pixel, ensuring that the final product not only meets but exceeds expectations. This project demonstrates my ability to blend cutting-edge design with seamless functionality, resulting in a website that is both visually stunning and user-friendly. Dive in to explore how I brought this vision to life and discover the transformative impact it can have on your digital presence.",
-    createdAt: "1st of May 2023",
+      "Step into the world of beauty and elegance with our team's latest achievement – a bespoke website for a nails and lashes salon, complete with user login functionality, appointment scheduling, and a seamless e-commerce platform for beauty products. This all-inclusive digital space invites users to personalize their salon experience through secure logins, ensuring a tailored and convenient journey. With a user-friendly appointment system, clients can effortlessly schedule their beauty sessions, while our integrated e-commerce section allows them to explore and purchase high-quality beauty products from the comfort of their homes. Join us in redefining the beauty industry's online presence, where sophistication meets convenience, and every click brings you closer to a pampering experience like no other.",
+    createdAt: "18th of August 2023",
   },
   {
     id: 6,
@@ -102,7 +102,7 @@ export const projectsData = [
       "/images/technologies/framer.webp",
     ],
     details:
-      "Introducing a showcase of my latest website project! This portfolio piece encapsulates my dedication to crafting captivating digital experiences that resonate with audiences. From concept to execution, I poured my creativity and expertise into every pixel, ensuring that the final product not only meets but exceeds expectations. This project demonstrates my ability to blend cutting-edge design with seamless functionality, resulting in a website that is both visually stunning and user-friendly. Dive in to explore how I brought this vision to life and discover the transformative impact it can have on your digital presence.",
-    createdAt: "1st of May 2023",
+      "Introducing our solemn yet dignified creation: a presentation website dedicated to funeral services, crafted with reverence and compassion. Our platform serves as a digital sanctuary, offering solace and support to those navigating the challenging process of arranging funeral ceremonies and commemorating the lives of loved ones. With a thoughtful and minimalist design, we provide essential information about our services, including burial and cremation options, memorial arrangements, and grief support resources. Through heartfelt testimonials and a discreet contact portal, we aim to provide comfort and guidance during this sensitive time. Join us as we honor the legacy of those we've lost, offering a beacon of hope and remembrance in their memory.",
+    createdAt: "20th of February 2023",
   },
 ];
