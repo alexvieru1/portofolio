9a0724cd7f13ce0271915bd27f7a8cdd73f86374
alexvieru1 <vieru.alexandruu@gmail.com>
2024-02-29 15:20:38 +0200

    clean-up for scroller

--- components/InfiniteCarousel.tsx
+++ components/InfiniteCarousel.tsx
@@ -26,47 +26,35 @@ const InfiniteCarousel = () => {
       >
         <h1>Areas of Expertise:</h1>
       </motion.div>
-      {isLargeScreen ? (
-        <div className="mt-10 w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
-          <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
-            {logos.map((logo, index) => (
-              <li key={index}>
-                <Image src={logo.src} width={100} height={100} alt={logo.alt} />
-              </li>
-            ))}
-          </ul>
-          <ul
-            className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
-            aria-hidden="true"
-          >
-            {logos.map((logo, index) => (
-              <li key={index}>
-                <Image src={logo.src} width={100} height={100} alt={logo.alt} />
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <div className="mt-10 w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
-          <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
-            {logos.map((logo, index) => (
-              <li key={index}>
-                <Image src={logo.src} width={50} height={50} alt={logo.alt} />
-              </li>
-            ))}
-          </ul>
-          <ul
-            className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
-            aria-hidden="true"
-          >
-            {logos.map((logo, index) => (
-              <li key={index}>
-                <Image src={logo.src} width={50} height={50} alt={logo.alt} />
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <div className="mt-10 w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
+        <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+          {logos.map((logo, index) => (
+            <li key={index}>
+              <Image
+                src={logo.src}
+                width={isLargeScreen ? 100 : 50}
+                height={isLargeScreen ? 100 : 50}
+                alt={logo.alt}
+              />
+            </li>
+          ))}
+        </ul>
+        <ul
+          className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+          aria-hidden="true"
+        >
+          {logos.map((logo, index) => (
+            <li key={index}>
+              <Image
+                src={logo.src}
+                width={isLargeScreen ? 100 : 50}
+                height={isLargeScreen ? 100 : 50}
+                alt={logo.alt}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
